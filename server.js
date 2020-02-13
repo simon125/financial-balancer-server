b5f4bcc0484c022faced57cc80e4a5ec228f9a94
Szymon <szymqwerty@gmail.com>
2020-02-13 01:24:18 +0100

    Ended basic verison of backend, still need to add simple possible to add users and getting data for current user

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env' });
 
@@ -11,6 +12,8 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1/expenses', require('./routes/expenses'));
 app.use('/api/v1/incomes', require('./routes/incomes'));
